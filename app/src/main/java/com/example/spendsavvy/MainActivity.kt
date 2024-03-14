a642c6a2643dfc709022237c2c46d87667831b90
Yung729 <leongkahyung@gmail.com>
2024-03-14 22:14:03 +0800

    login layout

--- app/src/main/java/com/example/spendsavvy/MainActivity.kt
+++ app/src/main/java/com/example/spendsavvy/MainActivity.kt
@@ -1,30 +1,45 @@
 package com.example.spendsavvy
 
-import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.unit.sp
+import com.example.spendsavvy.ui.theme.ButtonColor
+import com.example.spendsavvy.ui.theme.HeaderTitle
 import com.example.spendsavvy.ui.theme.SpendSavvyTheme
+import com.example.spendsavvy.ui.theme.poppinsFontFamily
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +49,11 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginPageScreen()
+                    LoginPageScreen(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(20.dp)
+                    )
                 }
             }
         }
@@ -45,6 +64,64 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun LoginPageScreen(modifier: Modifier = Modifier) {
 
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Login",
+            fontFamily = poppinsFontFamily,
+            textAlign = TextAlign.Center,
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+            color = HeaderTitle
+        )
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text(text = "User Name") },
+            leadingIcon = {
+                Icon(Icons.Default.Person, contentDescription = "person")
+            },
+            maxLines = 1,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp, top = 10.dp)
+        )
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text(text = "Password") },
+            leadingIcon = {
+                Icon(Icons.Default.Info, contentDescription = "password")
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp, top = 10.dp)
+        )
+
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.padding(bottom = 10.dp, top = 10.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = ButtonColor
+            )
+        ) {
+            Text(
+                text = "LOGIN",
+                textAlign = TextAlign.Center
+            )
+        }
+
+    }
+
 
 }
 
@@ -53,6 +130,6 @@ fun LoginPageScreen(modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     SpendSavvyTheme {
-        LoginPageScreen()
+
     }
 }
