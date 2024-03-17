803fc123b449d3da9be8965640ea02f79b9b80ca
Yung729 <leongkahyung@gmail.com>
2024-03-17 11:49:36 +0800

    show/hide password

--- app/src/main/java/com/example/spendsavvy/LoginScreen.kt
+++ app/src/main/java/com/example/spendsavvy/LoginScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -15,6 +16,7 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,8 +26,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,10 +43,17 @@ import com.example.spendsavvy.ui.theme.SpendSavvyTheme
 import com.example.spendsavvy.ui.theme.poppinsFontFamily
 
 @Composable
-fun LoginScreen(modifier: Modifier = Modifier,navController: NavController) {
+fun LoginScreen(modifier: Modifier = Modifier, navController: NavController) {
 
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+
+    var passwordVisible by remember { mutableStateOf(false) }
+
+    val passIcon = if (passwordVisible)
+        painterResource(id = R.drawable.show_pass)
+    else
+        painterResource(id = R.drawable.hide_pass)
 
     Column(
         modifier = modifier,
@@ -81,7 +93,18 @@ fun LoginScreen(modifier: Modifier = Modifier,navController: NavController) {
             leadingIcon = {
                 Icon(Icons.Default.Info, contentDescription = "password")
             },
+            trailingIcon = {
+                IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                    Icon(
+                        painter = passIcon,
+                        contentDescription = "PassIcon",
+                        modifier = Modifier.size(35.dp)
+                    )
+                }
+            },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            visualTransformation = if (passwordVisible) VisualTransformation.None
+            else PasswordVisualTransformation(),
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(bottom = 10.dp, top = 10.dp),
@@ -92,7 +115,7 @@ fun LoginScreen(modifier: Modifier = Modifier,navController: NavController) {
         Button(
             onClick = {
                 navController.navigate(route = Screen.SignUp.route)
-                      },
+            },
             modifier = Modifier.padding(bottom = 10.dp, top = 10.dp),
             colors = ButtonDefaults.buttonColors(
                 containerColor = ButtonColor
