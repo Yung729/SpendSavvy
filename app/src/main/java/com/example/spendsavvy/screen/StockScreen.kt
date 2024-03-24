8c4ab67267f41dfe39d94bf3088e22b923d233d7
Yung729 <leongkahyung@gmail.com>
2024-03-24 13:34:57 +0800

    Update StockScreen.kt

--- app/src/main/java/com/example/spendsavvy/screen/StockScreen.kt
+++ app/src/main/java/com/example/spendsavvy/screen/StockScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,9 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -33,8 +30,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.example.spendsavvy.animation.bounceClick
 import com.example.spendsavvy.component.HeaderTitle
 import com.example.spendsavvy.data.StockData
@@ -92,44 +87,52 @@ fun StockScreen(modifier: Modifier = Modifier) {
             color = com.example.spendsavvy.ui.theme.HeaderTitle
         )
 
-        StockList(stockList = StockData().loadStock())
+        Box(modifier = Modifier.fillMaxWidth()) {
+            StockList(stockList = StockData().loadStock())
 
-
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 20.dp)
-        ) {
-            Button(
-                onClick = { },
+            Box(
                 modifier = Modifier
-                    .padding(bottom = 10.dp, top = 10.dp)
-                    .bounceClick(),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.Black
-                )
+                    .align(Alignment.BottomCenter)
+                    .fillMaxWidth()
             ) {
-                Text(
-                    text = "Add Stock",
-                    textAlign = TextAlign.Center,
-                    color = Color.White
-                )
-            }
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Button(
+                        onClick = { },
+                        modifier = Modifier
+                            .padding(bottom = 10.dp)
+                            .bounceClick()
+                            .fillMaxWidth(),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Black
+                        )
+                    ) {
+                        Text(
+                            text = "Add Stock",
+                            textAlign = TextAlign.Center,
+                            color = Color.White
+                        )
+                    }
 
-            Button(
-                onClick = { },
-                modifier = Modifier
-                    .padding(bottom = 10.dp, top = 10.dp)
-                    .bounceClick(),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.White
-                )
-            ) {
-                Text(
-                    text = "Sell Stock",
-                    textAlign = TextAlign.Center,
-                    color = Color.Black
-                )
+                    Button(
+                        onClick = { },
+                        modifier = Modifier
+                            .padding(bottom = 10.dp)
+                            .bounceClick()
+                            .fillMaxWidth(),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.White
+                        )
+                    ) {
+                        Text(
+                            text = "Sell Stock",
+                            textAlign = TextAlign.Center,
+                            color = Color.Black
+                        )
+                    }
+                }
             }
         }
 
