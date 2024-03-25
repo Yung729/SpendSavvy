e419276df84a5a5eee1b61fa377909fb4aa3306e
Yihao24 <145582306+yihao0824@users.noreply.github.com>
2024-03-25 23:24:54 +0800

    settings screen 25/3/24

--- app/src/main/java/com/example/spendsavvy/screen/SettingsScreen.kt
+++ app/src/main/java/com/example/spendsavvy/screen/SettingsScreen.kt
@@ -1,19 +1,23 @@
 package com.example.spendsavvy.screen
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -28,6 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
@@ -56,22 +61,143 @@ import com.google.firebase.auth.auth
 fun SettingsScreen(modifier: Modifier = Modifier, navController: NavController) {
 
     Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
     ) {
-        Text(
-            text = "Settings",
-            fontFamily = poppinsFontFamily,
-            textAlign = TextAlign.Center,
-            fontSize = 40.sp,
-            fontWeight = FontWeight.Bold,
-            color = HeaderTitle
-        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Icon(
+                Icons.Default.KeyboardArrowLeft,
+                contentDescription = "Up",
+                modifier = Modifier.align(Alignment.CenterVertically)
+            )
 
-        Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = "Settings",
+                fontFamily = poppinsFontFamily,
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold,
+                color = HeaderTitle,
+                modifier = Modifier
+                    .padding(end = 130.dp)
+            )
+        }
+
+        Column(
+            modifier = modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Top,
+        ) {
+            Image(
+                painter = painterResource(R.drawable.add_image_icon),
+                contentDescription = "user profile image",
+                modifier = Modifier
+                    .size(100.dp)
+                    .padding(vertical = 16.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+
+            Text(
+                text = "User nameeee",
+                fontFamily = poppinsFontFamily,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Normal,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+            )
+
+            Row(
+                modifier = Modifier
+                    .padding(vertical = 5.dp)
+            ) {
+                Text(
+                    text = "User Information",
+                    fontFamily = poppinsFontFamily,
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.Normal,
+                    modifier = Modifier.padding(top = 16.dp)
+                )
+            }
+
+            List(R.drawable.profile_icon, "User Profile")
+            List(R.drawable.expense_icon, "Recent Expenses")
+
+            Row(
+                modifier = Modifier
+                    .padding(vertical = 5.dp)
+            ) {
+                Text(
+                    text = "Security",
+                    fontFamily = poppinsFontFamily,
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.Normal,
+                )
+            }
+
+            List(R.drawable.changepassword_icon, "Change Password")
+            List(R.drawable.forgotpassword_icon, "Forgot Password")
+
+            Row(
+                modifier = Modifier
+                    .padding(vertical = 5.dp)
+            ) {
+                Text(
+                    text = "General",
+                    fontFamily = poppinsFontFamily,
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.Normal,
+                )
+            }
+
+            List(R.drawable.bell_icon, "Notifications")
+            List(R.drawable.language_icon, "Language")
+            List(R.drawable.help_icon, "Help and Support")
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 5.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Logout",
+                    fontFamily = poppinsFontFamily,
+                    fontSize = 25.sp,
+                    color = Color.Red,
+                    fontWeight = FontWeight.Normal,
+                )
+            }
+        }
     }
 }
+
+@Composable
+fun List(imgId: Int, text: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp, horizontal = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = imgId),
+            contentDescription = "",
+            modifier = Modifier.size(30.dp)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Text(
+            text = text,
+            fontFamily = poppinsFontFamily,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Normal,
+            modifier = Modifier.weight(1f) // Occupy remaining space
+        )
+    }
+}
+
 
 
 @Preview(showBackground = true)
