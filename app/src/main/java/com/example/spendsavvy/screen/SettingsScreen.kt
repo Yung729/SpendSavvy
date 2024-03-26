e315517716ad6f5d23294e5ce064776fc6153946
Yihao24 <145582306+yihao0824@users.noreply.github.com>
2024-03-27 01:39:37 +0800

    asd

--- app/src/main/java/com/example/spendsavvy/screen/SettingsScreen.kt
+++ app/src/main/java/com/example/spendsavvy/screen/SettingsScreen.kt
@@ -1,29 +1,52 @@
 package com.example.spendsavvy.screen
 
-
+import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,15 +54,22 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.spendsavvy.R
+import com.example.spendsavvy.navigation.Screen
+import com.example.spendsavvy.ui.theme.ButtonColor
 import com.example.spendsavvy.ui.theme.HeaderTitle
 import com.example.spendsavvy.ui.theme.poppinsFontFamily
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 
 @Composable
 fun SettingsScreen(modifier: Modifier = Modifier, navController: NavController) {
 
     Column(
-        modifier = modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 10.dp),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.Start
     ) {
@@ -50,88 +80,85 @@ fun SettingsScreen(modifier: Modifier = Modifier, navController: NavController) 
             Icon(
                 Icons.Default.KeyboardArrowLeft,
                 contentDescription = "Up",
-                modifier = Modifier.align(Alignment.CenterVertically)
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .size(40.dp)
             )
-
             Text(
                 text = "Settings",
                 fontFamily = poppinsFontFamily,
                 fontSize = 25.sp,
-                fontWeight = FontWeight.Bold,
+                fontWeight = FontWeight.SemiBold,
                 color = HeaderTitle,
                 modifier = Modifier
                     .padding(end = 130.dp)
             )
         }
-
+//sadf
         Column(
-            modifier = modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
             verticalArrangement = Arrangement.Top,
         ) {
             Image(
                 painter = painterResource(R.drawable.add_image_icon),
-                contentDescription = "user profile image",
+                contentDescription = "User profile image",
                 modifier = Modifier
-                    .size(100.dp)
+                    .size(125.dp)
                     .padding(vertical = 16.dp)
                     .align(Alignment.CenterHorizontally)
+                    .clip(CircleShape)
+                    .background(Color.Gray),
             )
 
             Text(
                 text = "User nameeee",
-                fontFamily = poppinsFontFamily,
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Normal,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Medium,
                 modifier = Modifier
                     .align(Alignment.CenterHorizontally)
             )
 
             Row(
-                modifier = Modifier
-                    .padding(vertical = 5.dp)
+                modifier = Modifier.padding(vertical = 5.dp)
             ) {
                 Text(
-                    text = "User Information",
-                    fontFamily = poppinsFontFamily,
-                    fontSize = 25.sp,
-                    fontWeight = FontWeight.Normal,
-                    modifier = Modifier.padding(top = 16.dp)
+                    text = "Personal Info",
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Medium
                 )
             }
 
-            List(R.drawable.profile_icon, "User Profile")
-            List(R.drawable.expense_icon, "Recent Expenses")
+            List(R.drawable.profile_icon, "User Profile",navController, Screen.MyProfile.route)
+//            List(R.drawable.expense_icon, "Recent Expenses",navController, Screen.Stock.route)
 
             Row(
-                modifier = Modifier
-                    .padding(vertical = 5.dp)
+                modifier = Modifier.padding(vertical = 5.dp)
             ) {
                 Text(
                     text = "Security",
-                    fontFamily = poppinsFontFamily,
-                    fontSize = 25.sp,
-                    fontWeight = FontWeight.Normal,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Medium,
                 )
             }
 
-            List(R.drawable.changepassword_icon, "Change Password")
-            List(R.drawable.forgotpassword_icon, "Forgot Password")
+            List(R.drawable.changepassword_icon, "Change Password",navController, Screen.Stock.route)
+            List(R.drawable.forgotpassword_icon, "Forgot Password",navController, Screen.Stock.route)
 
             Row(
-                modifier = Modifier
-                    .padding(vertical = 5.dp)
+                modifier = Modifier.padding(vertical = 5.dp)
             ) {
                 Text(
                     text = "General",
-                    fontFamily = poppinsFontFamily,
-                    fontSize = 25.sp,
-                    fontWeight = FontWeight.Normal,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Medium,
                 )
             }
 
-            List(R.drawable.bell_icon, "Notifications")
-            List(R.drawable.language_icon, "Language")
-            List(R.drawable.help_icon, "Help and Support")
+            List(R.drawable.bell_icon, "Notifications",navController, Screen.Stock.route)
+            List(R.drawable.language_icon, "Languages",navController, Screen.Stock.route)
+            List(R.drawable.help_icon, "Help and Support",navController, Screen.Stock.route)
 
             Row(
                 modifier = Modifier
@@ -141,10 +168,9 @@ fun SettingsScreen(modifier: Modifier = Modifier, navController: NavController) 
             ) {
                 Text(
                     text = "Logout",
-                    fontFamily = poppinsFontFamily,
-                    fontSize = 25.sp,
+                    fontSize = 20.sp,
                     color = Color.Red,
-                    fontWeight = FontWeight.Normal,
+                    fontWeight = FontWeight.Medium,
                 )
             }
         }
@@ -152,30 +178,44 @@ fun SettingsScreen(modifier: Modifier = Modifier, navController: NavController) 
 }
 
 @Composable
-fun List(imgId: Int, text: String) {
+fun List(imgId: Int, text: String, navController: NavController, route: String) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
+            .clickable {navController.navigate(route = route)}
             .padding(vertical = 8.dp, horizontal = 16.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Image(
             painter = painterResource(id = imgId),
             contentDescription = "",
-            modifier = Modifier.size(30.dp)
+            modifier = Modifier
+                .size(30.dp)
         )
         Spacer(modifier = Modifier.width(8.dp))
 
         Text(
             text = text,
-            fontFamily = poppinsFontFamily,
             fontSize = 16.sp,
-            fontWeight = FontWeight.Normal,
-            modifier = Modifier.weight(1f) // Occupy remaining space
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier.weight(1f),
+            color = Color.Gray
+
         )
     }
+    LineDivider()
 }
-
+@Composable
+fun LineDivider() {
+    Divider(
+        color = Color.Gray,
+        thickness = 0.8.dp,
+        modifier = Modifier
+            .padding(vertical = 10.dp)
+            .padding(horizontal = 10.dp)
+            .fillMaxWidth()
+    )
+}
 
 
 @Preview(showBackground = true)
