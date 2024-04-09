4ad388bf213d3cb3096be3cde5447262dea688c3
Joseph Lai <137158285+B1ockchainEngineer@users.noreply.github.com>
2024-04-09 13:21:00 +0800

    1

--- app/src/main/java/com/example/spendsavvy/screen/Money.kt
+++ app/src/main/java/com/example/spendsavvy/screen/Money.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SegmentedButton
@@ -195,7 +196,7 @@ fun MoneyPopUpScreen(
                         )
                     ) {
                     
-                }
+                    }
                 }
             }
         }
