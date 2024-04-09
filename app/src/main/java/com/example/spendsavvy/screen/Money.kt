5642f68923b72099b475fffd779bb10c32eab6ea
Joseph Lai <137158285+B1ockchainEngineer@users.noreply.github.com>
2024-04-09 08:18:45 +0800

    otw

--- app/src/main/java/com/example/spendsavvy/screen/Money.kt
+++ app/src/main/java/com/example/spendsavvy/screen/Money.kt
@@ -1,5 +1,9 @@
 package com.example.spendsavvy.screen
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,15 +15,27 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonColors
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.font.FontWeight
@@ -27,6 +43,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.spendsavvy.animation.bounceClick
@@ -35,7 +53,10 @@ import com.example.spendsavvy.model.BankAccount
 import com.example.spendsavvy.ui.theme.poppinsFontFamily
 
 @Composable
-fun MoneyScreen(modifier: Modifier){
+fun MoneyScreen(modifier: Modifier) {
+
+    var isDialogPopUp = remember { mutableStateOf(false) }
+
 
     Column(modifier = Modifier.padding(10.dp)) {
         Spacer(modifier = Modifier.height(25.dp))
@@ -56,7 +77,7 @@ fun MoneyScreen(modifier: Modifier){
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
-        ){
+        ) {
             Column {
                 Text(
                     text = "Cash Money",
@@ -92,7 +113,8 @@ fun MoneyScreen(modifier: Modifier){
                 fontFamily = poppinsFontFamily
             )
 
-            Text(text = "3 Accounts", /*calculate acc*/
+            Text(
+                text = "3 Accounts", /*calculate acc*/
                 fontFamily = poppinsFontFamily
             )
         }
@@ -115,7 +137,7 @@ fun MoneyScreen(modifier: Modifier){
                     horizontalAlignment = Alignment.End
                 ) {
                     Button(
-                        onClick = { /*TODO*/ },
+                        onClick = { isDialogPopUp.value = true },
                         modifier = Modifier
                             .padding(bottom = 10.dp)
                             .bounceClick()
@@ -134,11 +156,56 @@ fun MoneyScreen(modifier: Modifier){
                 }
             }
         }
+
+        if (isDialogPopUp.value)
+            MoneyPopUpScreen(onCancelClick = { isDialogPopUp.value = false }, {})
     }
 
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnrememberedMutableState", "RememberReturnType")
+@Composable
+fun MoneyPopUpScreen(
+    onCancelClick: () -> Unit,          //nothing to be returned
+    onConfirmClick: () -> Unit
+) {
+    val options = mutableStateListOf<String>("Cash", "Bank")
+    var selectedIndex by remember {
+        mutableStateOf(0)
+    }
+
+    Dialog(
+        onDismissRequest = { onCancelClick() },
+        properties = DialogProperties(
+            usePlatformDefaultWidth = false
+        )
+    ) {
+        Card(
+            shape = RoundedCornerShape(15.dp),
+            modifier = Modifier
+                .fillMaxWidth(0.85f)
+                .border(1.dp, color = Color.Gray, shape = RoundedCornerShape(15.dp))
+                .shadow(elevation = 15.dp)
+        ) {
+            SingleChoiceSegmentedButtonRow {
+                options.forEachIndexed{
+                    index, option ->
+                    SegmentedButton(
+                    selected = selectedIndex == index,
+                    onClick = { selectedIndex = index },
+                    shape = SegmentedButtonDefaults.itemShape(
+                        index = index, count = options.size
+                        )
+                    ) {
+                    
+                }
+                }
+            }
+        }
+    }
+}
 
 
 @Preview(showBackground = true)
