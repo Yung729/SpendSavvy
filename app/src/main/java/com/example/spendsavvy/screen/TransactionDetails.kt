165dcffbcbaabc54f72396124fd2a22e995030c5
Yung729 <leongkahyung@gmail.com>
2024-04-27 13:09:08 +0800

    Update TransactionDetails.kt

--- app/src/main/java/com/example/spendsavvy/screen/TransactionDetails.kt
+++ app/src/main/java/com/example/spendsavvy/screen/TransactionDetails.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,6 +31,15 @@ import com.example.spendsavvy.components.bounceClick
 import com.example.spendsavvy.models.Category
 import com.example.spendsavvy.models.Transactions
 import com.example.spendsavvy.viewModels.OverviewViewModel
+import com.maxkeppeker.sheets.core.models.base.rememberSheetState
+import com.maxkeppeler.sheets.calendar.CalendarDialog
+import com.maxkeppeler.sheets.calendar.models.CalendarConfig
+import com.maxkeppeler.sheets.calendar.models.CalendarSelection
+import com.maxkeppeler.sheets.calendar.models.CalendarStyle
+import java.text.SimpleDateFormat
+import java.time.ZoneId
+import java.util.Date
+import java.util.Locale
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnrememberedMutableState")
@@ -49,7 +59,9 @@ fun TransactionDetail(
         mutableStateOf(transactions.description)
     }
 
-
+    val calendarState = rememberSheetState()
+    val updatedDate = remember { mutableStateOf<Date>(transactions.date) }
+    val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
 
 
     Column(
@@ -86,6 +98,23 @@ fun TransactionDetail(
         )
 
         Spacer(modifier = Modifier.height(8.dp))
+
+        CalendarDialog(
+            state = calendarState,
+            config = CalendarConfig(
+                monthSelection = true,
+                yearSelection = true,
+                style = CalendarStyle.MONTH
+            ),
+            selection = CalendarSelection.Date { date ->
+                val selectedDateValue =
+                    Date.from(date.atStartOfDay(ZoneId.systemDefault()).toInstant())
+                updatedDate.value = selectedDateValue
+            })
+
+        OutlinedButton(onClick = { calendarState.show() }) {
+            Text(text = updatedDate.value.let { dateFormat.format(it) } ?: "Select Date")
+        }
 
         OutlinedTextField(
             value = updatedTransactionDescription,
