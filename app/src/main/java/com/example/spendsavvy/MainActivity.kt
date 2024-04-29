d08e7fbcf30a99d3442015f8bb5aaa51cdc9bf20
Yung729 <leongkahyung@gmail.com>
2024-04-29 23:46:06 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/spendsavvy/MainActivity.kt
+++ app/src/main/java/com/example/spendsavvy/MainActivity.kt
@@ -3,15 +3,8 @@ package com.example.spendsavvy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import com.example.spendsavvy.navigation.SetupNavGraph
 import com.example.spendsavvy.ui.theme.SpendSavvyTheme
-import com.google.firebase.Firebase
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.auth
-
-
 
 
 class MainActivity : ComponentActivity() {
