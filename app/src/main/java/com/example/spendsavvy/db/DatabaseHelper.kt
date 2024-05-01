f45707d78a270eede2de5175ae3056ad153f5ef2
Yung729 <leongkahyung@gmail.com>
2024-05-01 17:02:07 +0800

    a

--- app/src/main/java/com/example/spendsavvy/db/DatabaseHelper.kt
+++ app/src/main/java/com/example/spendsavvy/db/DatabaseHelper.kt
@@ -342,7 +342,7 @@ class DatabaseHelper(context: Context) :
 
     companion object {
         private const val DATABASE_NAME = "Local_Database"
-        private const val DATABASE_VERSION = 7
+        private const val DATABASE_VERSION = 9
     }
 }
 
