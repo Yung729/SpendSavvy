66deda7aba02640408ed35271f8cc1a785f9202d
Yung729 <leongkahyung@gmail.com>
2024-05-08 21:59:59 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,7 +83,8 @@ dependencies {
     implementation ("com.maxkeppeler.sheets-compose-dialogs:calendar:1.0.2")
     implementation("io.coil-kt:coil-compose:2.2.0")
     implementation ("androidx.compose.material:material:1.6.7")
-    implementation ("androidx.compose.runtime:runtime-livedata:1.6.6")
+    implementation ("androidx.compose.runtime:runtime-livedata:1.6.7")
     implementation ("com.google.accompanist:accompanist-permissions:0.34.0")
-    implementation ("androidx.datastore:datastore-preferences:1.1.0")
+    implementation ("androidx.datastore:datastore-preferences:1.1.1")
+    implementation ("com.google.android.gms:play-services-auth:21.1.1")
 }
