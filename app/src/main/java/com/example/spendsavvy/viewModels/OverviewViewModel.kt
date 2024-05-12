e35d0f9eef37fb99bc69d8bbe8dd115ae07ff15d
Yung729 <leongkahyung@gmail.com>
2024-05-12 10:20:54 +0800

    date solve

--- app/src/main/java/com/example/spendsavvy/viewModels/OverviewViewModel.kt
+++ app/src/main/java/com/example/spendsavvy/viewModels/OverviewViewModel.kt
@@ -71,6 +71,15 @@ class OverviewViewModel(
     }.time
 
     init {
+        if (selectedDateFromUser.value == null) {
+            dateViewModel.setSelectedDate(Calendar.getInstance().apply {
+                time = Date()
+                set(Calendar.HOUR_OF_DAY, 0)
+                set(Calendar.MINUTE, 0)
+                set(Calendar.SECOND, 0)
+                set(Calendar.MILLISECOND, 0)
+            }.time)
+        }
         getTransactionRecord()
     }
 
