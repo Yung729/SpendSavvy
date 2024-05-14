6a78a4f4a5a7e2ecb91981ed4e2da6893b76d4bc
Yung729 <leongkahyung@gmail.com>
2024-05-14 21:45:31 +0800

    merge

--- app/src/main/java/com/example/spendsavvy/MainActivity.kt
+++ app/src/main/java/com/example/spendsavvy/MainActivity.kt
@@ -1,10 +1,5 @@
 package com.example.spendsavvy
 
-import android.annotation.SuppressLint
-import android.app.AlarmManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,8 +7,6 @@ import androidx.compose.ui.platform.LocalContext
 import com.example.spendsavvy.navigation.SetupNavGraph
 import com.example.spendsavvy.ui.theme.SpendSavvyTheme
 import com.example.spendsavvy.ui.theme.rememberWindowSize
-import com.example.spendsavvy.worker.BillsAlarmReceiver
-import java.util.Calendar
 
 
 class MainActivity : ComponentActivity() {
