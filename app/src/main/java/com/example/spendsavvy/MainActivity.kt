0bb32bea0605debf416aa05ec33d57d2144cf831
Yung729 <leongkahyung@gmail.com>
2024-05-15 13:17:07 +0800

    a

--- app/src/main/java/com/example/spendsavvy/MainActivity.kt
+++ app/src/main/java/com/example/spendsavvy/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.spendsavvy
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.ui.platform.LocalContext
 import com.example.spendsavvy.navigation.SetupNavGraph
 import com.example.spendsavvy.ui.theme.SpendSavvyTheme
@@ -12,6 +14,7 @@ import com.example.spendsavvy.ui.theme.rememberWindowSize
 class MainActivity : ComponentActivity() {
 
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
